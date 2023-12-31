951f55db6f401a045f8e68df2a1c320e22152e06
Jordan Romero <jordan.t.romero@gmail.com>
2023-12-30 18:28:30 -0600

    Renames the scene, adds particle effects

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -1,14 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] float reloadDelay = 0.5f;
+    [SerializeField] ParticleSystem crashEffect;
     void OnTriggerEnter2D(Collider2D other)
     {
         if(other.tag == "Ground")
         {
-            Debug.Log("You lose!");
+            crashEffect.Play();
+            Invoke("ReloadScene", reloadDelay);
         }
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
